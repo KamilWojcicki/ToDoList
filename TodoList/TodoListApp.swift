ed968ee7747a9613776203e65394b459c5d201a2
Kamil Wojcicki <kamilwojcickinjr@gmail.com>
2023-05-17 12:14:55 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Kamil Wójcicki on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
